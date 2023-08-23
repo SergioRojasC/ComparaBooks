548a1697a36e5abf1f37eb5b4fb6d89fffcc1e3c
Rojas Castro Sergio Vladimir (DMD-Metropolitana) <srojasc@hogardecristo.cl>
2023-08-23 16:27:00 -0400

    Se crea proyecto

---
+++ ComparaBooks/ComparaBooks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComparaBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
